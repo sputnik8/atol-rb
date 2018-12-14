76797db8934ebbc4b18aeb463de0611ea22f9dcc
GeorgeGorbanev <georgegorbanev@gmail.com>
2018-12-14 17:50:07 +0300

    0.4.1

--- lib/atol/version.rb
+++ lib/atol/version.rb
@@ -1,6 +1,6 @@
 module Atol
   module Version
     API = 'v4'.freeze
-    LIB = '0.4'.freeze
+    LIB = '0.4.1'.freeze
   end
 end
--- spec/atol/version_spec.rb
+++ spec/atol/version_spec.rb
@@ -3,5 +3,5 @@ require './lib/atol/version'
 describe Atol::Version do
   it { expect(Atol::Version).to be_a Module }
   it('API eql v4') { expect(Atol::Version::API).to eql 'v4' }
-  it('LIB eql 0.4') { expect(Atol::Version::LIB).to eql '0.4' }
+  it('LIB eql 0.4.1') { expect(Atol::Version::LIB).to eql '0.4.1' }
 end
